98348d960507d84b87130347db449152f3700cbc
Kade Chambers <kgbchambers@gmail.com>
2022-05-06 00:54:03 -0700

    Save edits

--- Assets/Scripts/Fabricator.cs
+++ Assets/Scripts/Fabricator.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class Fabricator : MonoBehaviour
 {
 
-    private int _robotCounter;
+    public int _robotCounter;
 
     private Transform spawnLocation;
 
--- Assets/Scripts/UpgradeManager.cs
+++ Assets/Scripts/UpgradeManager.cs
@@ -6,29 +6,67 @@ using System.Linq;
 public class UpgradeManager : Singleton<UpgradeManager>
 {
     public List<Upgrade> upgrades;
+    private int conveyorTier;
+    private int robotTier;
+    private int fabricatorTier;
+    private int scrapRechargeTier;
+    private int scrapCapTier;
+
 
     private void Start()
     {
+        if (!PlayerPrefs.HasKey("SaveCheck"))
+        {
+            PlayerPrefs.SetInt("SaveCheck", 1);
+            PlayerPrefs.SetInt("scrapRechargeTier", scrapRechargeTier);
+            PlayerPrefs.SetInt("scrapCapTier", scrapCapTier);
+            PlayerPrefs.SetInt("conveyorTier", conveyorTier);
+            PlayerPrefs.SetInt("fabricatorTier", fabricatorTier);
+            PlayerPrefs.SetInt("robotTier", robotTier);
+            PlayerPrefs.Save();
+        }
+        else
+        {
+            scrapRechargeTier = PlayerPrefs.GetInt("scrapRechargeTier");
+            scrapCapTier = PlayerPrefs.GetInt("scrapCapTier");
+            conveyorTier = PlayerPrefs.GetInt("conveyorTier");
+            fabricatorTier = PlayerPrefs.GetInt("fabricatorTier");
+            robotTier = PlayerPrefs.GetInt("robotTier");
+        }
+
+        foreach (Upgrade upgrade in upgrades)
+        {
+            //upgrade.cost =
+        }
+
         upgrades = upgrades.OrderBy(up => up.cost).ToList();
-        
+
     }
+
+
+    public void RecalculateCost()
+    {
+
+    }
+
+
 
 
     public void UpgradeScrapCap(string op, float mod, float cost)
     {
         if (GameManager.instance.spendCash(cost))
         {
-            if(op == "x ")
+            if (op == "x ")
             {
                 GameManager.instance.scrapCap *= mod;
             }
-            else if(op == "+ ")
+            else if (op == "+ ")
             {
                 GameManager.instance.scrapCap += mod;
             }
         }
 
-        
+
     }
 
     public void UpgradeScrapRecharge(string op, float mod, float cost)
